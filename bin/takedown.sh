5149771fbc23c3b1bb1715fbe25b3f8fbdd40b91
Harrison Powers <fijimunkii@gmail.com>
2015-10-09 14:34:05 -0400

    Only stop the container if it exists

--- bin/takedown.sh
+++ bin/takedown.sh
@@ -8,5 +8,8 @@ echo "Taking down $server"
 echo "$HUBOT_SSH_KEY" > /tmp/id_rsa
 
 ssh $user@$server -i /tmp/id_rsa -o StrictHostKeyChecking=no bash -c "'
-  docker ps -q --filter label=name=$server | xargs docker stop &> /dev/null
+  CONTAINER=\`docker ps -q --filter label=name=$server\`
+  if [ -n "$CONTAINER" ]
+  then docker stop $CONTAINER
+  fi
 '"
