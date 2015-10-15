c785dffde971330aa5b44a788e27ad5c038f5a5b
Harrison Powers <fijimunkii@gmail.com>
2015-10-15 16:19:20 -0400

    Add docker image removal to takedown

--- bin/takedown.sh
+++ bin/takedown.sh
@@ -8,5 +8,6 @@ echo "Taking down $server"
 echo "$HUBOT_SSH_KEY" > /tmp/id_rsa
 
 ssh $user@$server -i /tmp/id_rsa -o StrictHostKeyChecking=no bash -c "'
-  docker stop \`docker ps -q --filter label=name=$server\` &> /dev/null
+  docker stop \`docker ps -q --filter label=name=$server\` 2>/dev/null | docker rm 2>/dev/null
+  docker rmi $server 2>/dev/null
 '"
